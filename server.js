f271a861c3f0a148b87825e66734a9005704dbe2
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-02-18 21:23:38 -0800

    Pretty much just touched EVERYTHING and made the theme lighter. Haven't gotten around to the desktop sidebar though

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path:'./.env'});
+	require('dotenv').config({path: './.env'});
 }
 
 const express = require('express')
@@ -16,14 +16,15 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public')) 
+app.use(express.static('public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.HACKTHECLOUD2_URL, {
-useNewUrlParser: true, useUnifiedTopology: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.HACKTHECLOUD2_URL, {
+// 	useNewUrlParser: true, useUnifiedTopology: true
+// })
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/about', aboutRouter)
